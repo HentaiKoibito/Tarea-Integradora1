2d81a8c58ec2967df98f28b78bd25604c4200901
juanj <juanjoserojasmayor12@gmail.com>
2021-03-29 18:11:29 -0500

    ActionSelector2

---
+++ Tarea-Integradora1/src/ui/Main.java
@@ -0,0 +1,19 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	public  void main(String[] args) {
+		
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		
+	}
+
+}
